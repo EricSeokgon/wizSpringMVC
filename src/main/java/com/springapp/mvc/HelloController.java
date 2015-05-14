39c56fbc750b4d798bdb7d680a0ee9df00d29d29
hadaslee <leesk55@gmail.com>
2015-05-14 14:10:05 +0900

    요청 처리 메소드

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,16 +1,39 @@
 package com.springapp.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+    @RequestMapping("/board/view")
+    public String view() {
+        return "board/view";
+    }
+
+    @RequestMapping("/board/content")
+    public String content(Model model) {
+
+        model.addAttribute("id", "abcde");
+        return "board/content";
+    }
+
+/*    @RequestMapping("/board/reply")
+    public String reply() {
+
+       /ModelAndView mv = new ModelAndView();
+        mv.addAllObjects("id", 30);
+        mv.setViewName("board/reply");
+        return mv;
+    }*/
 }
