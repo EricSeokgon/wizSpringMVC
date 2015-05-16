2bdef2282b3662b08e66938de403f0c431993fea
hadesleee <leesk55@gmail.com>
2015-05-17 04:59:32 +0900

    TicketDao init

---
+++ src/main/java/com/springapp/mvc/dao/TicketDao.java
@@ -0,0 +1,13 @@
+package com.springapp.mvc.dao;
+
+/**
+ * Project: wizSpringMVC
+ * FileName: TicketDao
+ * Date: 2015-05-17
+ * Time: ¿ÀÀü 4:59
+ * Author: hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicketDao {
+}
