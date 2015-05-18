4ecb6fcdbe7493c8d3eb60d99e502d4d296fca16
hadaslee <leesk55@gmail.com>
2015-05-18 15:36:20 +0900

    security loginform fail page add

--- src/main/java/com/spring_security_pjt/HController.java
+++ src/main/java/com/spring_security_pjt/HController.java
@@ -1,39 +1,28 @@
 package com.spring_security_pjt;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
  */
-package com.javalec.spring_security_pjt;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class HController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HController.class);
 
 	/**
 	 * Simply selects the home view to render by returning its name.
--- src/main/webapp/WEB-INF/pages/security/loginForm.jsp
+++ src/main/webapp/WEB-INF/pages/security/loginForm.jsp
@@ -9,8 +9,16 @@
 </head>
 <body>
 <h1>loginForm.jsp</h1>
-
-<form action="<c:url value="j_spring_security_check" />" method="post">
+<c:url value="j_spring_security_check" var="loginUrl"/>
+<form action="${loginUrl}" method="post">
+	<c:if test="${param.ng != null}">
+	<p>
+		LogIn NG! <br />
+		<c:if test="${SPRING_SECURITY_LAST_EXCEPTION != NULL}">
+			message : <c:out value="${SPRING_SECURITY_LAST_EXCEPTION.message}" />
+		</c:if>
+	</p>
+	</c:if>
 	ID : <input type="text" name="j_username"> <br />
 	PW : <input type="text" name="j_password"> <br />
 	<input type="submit" value="LOGIN"> <br />
